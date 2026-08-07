ee30b000e723cb5feeb882aa82e9e8fffb87981e
matheus <matverialmeida2@gmail.com>
2026-08-07 16:25:58 -0300

    API completa

--- Movies.API/Controllers/AuthController.cs
+++ Movies.API/Controllers/AuthController.cs
@@ -1,6 +1,95 @@
-﻿namespace Movies.API.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.DTOs;
+
+namespace Movies.API.Controllers
 {
-    public class AuthController
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        //Criar usuario
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterDto dto)
+        {
+            if (dto.Password != dto.ConfirmPassword)
+                return BadRequest(new { message = "As senhas são diferentes!" });
+
+            var user = new IdentityUser
+            {
+                UserName = dto.Email,
+                Email = dto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(equals => equals.Description);
+                return BadRequest(new { message = "Erro ao registrar!", errors });
+            }
+
+            return Ok(new { message = "Usuario registrado!" });
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto dto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                dto.Email, dto.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+                return Unauthorized(new { message = "Email ou senha invalidos!" });
+
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            var roles = await _userManager.GetRolesAsync(user!);
+
+            return Ok(new UserDto
+            {
+               Id = user!.Id,
+               Email = user.Email!,
+               Roles = roles
+            });
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<ActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(new { message = "logout realizado com sucesso!" });
+        }
+
+        [HttpGet("me")]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> Me()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return Unauthorized(new { message = "Usuario não encontrado" });
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok(new UserDto
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Roles = roles
+            });
+        }
     }
 }
