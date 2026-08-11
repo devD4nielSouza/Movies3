f8aa99682911a1cc44164287ed24434618cf8761
Matheus <matverialmeida2@gmail.com>
2026-08-11 17:28:57 -0300

    Inicio MovieDialog

---
+++ Movies.Desktop/Forms/MovieFormDialog.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Movies.Desktop.Forms
+{
+    partial class MovieFormDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            cmbCategoria = new ComboBox();
+            btnCancelar = new Guna.UI2.WinForms.Guna2Button();
+            btnSalvar = new Guna.UI2.WinForms.Guna2Button();
+            txtDescricao = new Guna.UI2.WinForms.Guna2TextBox();
+            txtCoverUrl = new Guna.UI2.WinForms.Guna2TextBox();
+            txtAno = new Guna.UI2.WinForms.Guna2TextBox();
+            txtTitulo = new Guna.UI2.WinForms.Guna2TextBox();
+            lblCampCategoria = new Label();
+            lblCampCover = new Label();
+            lblCampAno = new Label();
+            lblCampDesc = new Label();
+            lblCampTitulo = new Label();
+            lblTituloForm = new Label();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            label1 = new Label();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // cmbCategoria
+            // 
+            cmbCategoria.BackColor = Color.White;
+            cmbCategoria.ForeColor = Color.Black;
+            cmbCategoria.FormattingEnabled = true;
+            cmbCategoria.Location = new Point(29, 424);
+            cmbCategoria.Name = "cmbCategoria";
+            cmbCategoria.Size = new Size(473, 23);
+            cmbCategoria.TabIndex = 18;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.BorderRadius = 10;
+            btnCancelar.CustomizableEdges = customizableEdges1;
+            btnCancelar.DisabledState.BorderColor = Color.DarkGray;
+            btnCancelar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCancelar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCancelar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCancelar.FillColor = Color.Gray;
+            btnCancelar.Font = new Font("Segoe UI", 9F);
+            btnCancelar.ForeColor = Color.White;
+            btnCancelar.Location = new Point(184, 543);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnCancelar.Size = new Size(95, 45);
+            btnCancelar.TabIndex = 16;
+            btnCancelar.Text = "Cancelar";
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.BorderRadius = 10;
+            btnSalvar.CustomizableEdges = customizableEdges3;
+            btnSalvar.DisabledState.BorderColor = Color.DarkGray;
+            btnSalvar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSalvar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSalvar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSalvar.FillColor = Color.Purple;
+            btnSalvar.Font = new Font("Segoe UI", 9F);
+            btnSalvar.ForeColor = Color.White;
+            btnSalvar.Location = new Point(29, 543);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnSalvar.Size = new Size(124, 45);
+            btnSalvar.TabIndex = 17;
+            btnSalvar.Text = "Criar";
+            // 
+            // txtDescricao
+            // 
+            txtDescricao.BackColor = Color.Transparent;
+            txtDescricao.BorderColor = Color.DarkGray;
+            txtDescricao.BorderRadius = 6;
+            txtDescricao.CustomizableEdges = customizableEdges5;
+            txtDescricao.DefaultText = "";
+            txtDescricao.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtDescricao.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtDescricao.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtDescricao.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtDescricao.FillColor = Color.WhiteSmoke;
+            txtDescricao.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtDescricao.Font = new Font("Segoe UI", 9F);
+            txtDescricao.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtDescricao.Location = new Point(29, 156);
+            txtDescricao.Name = "txtDescricao";
+            txtDescricao.PlaceholderForeColor = Color.Gray;
+            txtDescricao.PlaceholderText = "Descrição do filme...";
+            txtDescricao.SelectedText = "";
+            txtDescricao.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtDescricao.Size = new Size(473, 75);
+            txtDescricao.TabIndex = 11;
+            txtDescricao.TextOffset = new Point(0, -25);
+            // 
+            // txtCoverUrl
+            // 
+            txtCoverUrl.BackColor = Color.Transparent;
+            txtCoverUrl.BorderColor = Color.DarkGray;
+            txtCoverUrl.BorderRadius = 6;
+            txtCoverUrl.CustomizableEdges = customizableEdges7;
+            txtCoverUrl.DefaultText = "";
+            txtCoverUrl.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCoverUrl.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCoverUrl.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCoverUrl.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCoverUrl.FillColor = Color.WhiteSmoke;
+            txtCoverUrl.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCoverUrl.Font = new Font("Segoe UI", 9F);
+            txtCoverUrl.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCoverUrl.Location = new Point(29, 367);
+            txtCoverUrl.Name = "txtCoverUrl";
+            txtCoverUrl.PlaceholderForeColor = Color.Gray;
+            txtCoverUrl.PlaceholderText = "https://...";
+            txtCoverUrl.SelectedText = "";
+            txtCoverUrl.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtCoverUrl.Size = new Size(473, 36);
+            txtCoverUrl.TabIndex = 12;
+            // 
+            // txtAno
+            // 
+            txtAno.BackColor = Color.Transparent;
+            txtAno.BorderColor = Color.DarkGray;
+            txtAno.BorderRadius = 6;
+            txtAno.CustomizableEdges = customizableEdges9;
+            txtAno.DefaultText = "";
+            txtAno.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtAno.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtAno.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtAno.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtAno.FillColor = Color.WhiteSmoke;
+            txtAno.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtAno.Font = new Font("Segoe UI", 9F);
+            txtAno.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtAno.Location = new Point(29, 252);
+            txtAno.Name = "txtAno";
+            txtAno.PlaceholderForeColor = Color.Gray;
+            txtAno.PlaceholderText = "Ex: 2024";
+            txtAno.SelectedText = "";
+            txtAno.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            txtAno.Size = new Size(473, 36);
+            txtAno.TabIndex = 13;
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.BackColor = Color.Transparent;
+            txtTitulo.BorderColor = Color.DarkGray;
+            txtTitulo.BorderRadius = 6;
+            txtTitulo.CustomizableEdges = customizableEdges11;
+            txtTitulo.DefaultText = "";
+            txtTitulo.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtTitulo.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtTitulo.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtTitulo.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtTitulo.FillColor = Color.WhiteSmoke;
+            txtTitulo.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTitulo.Font = new Font("Segoe UI", 9F);
+            txtTitulo.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTitulo.Location = new Point(29, 88);
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.PlaceholderForeColor = Color.Gray;
+            txtTitulo.PlaceholderText = "Ex: Lala Land";
+            txtTitulo.SelectedText = "";
+            txtTitulo.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            txtTitulo.Size = new Size(473, 36);
+            txtTitulo.TabIndex = 14;
+            // 
+            // lblCampCategoria
+            // 
+            lblCampCategoria.AutoSize = true;
+            lblCampCategoria.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCampCategoria.Location = new Point(29, 406);
+            lblCampCategoria.Name = "lblCampCategoria";
+            lblCampCategoria.Size = new Size(80, 15);
+            lblCampCategoria.TabIndex = 5;
+            lblCampCategoria.Text = "CATEGORIA *";
+            // 
+            // lblCampCover
+            // 
+            lblCampCover.AutoSize = true;
+            lblCampCover.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCampCover.Location = new Point(29, 349);
+            lblCampCover.Name = "lblCampCover";
+            lblCampCover.Size = new Size(82, 15);
+            lblCampCover.TabIndex = 6;
+            lblCampCover.Text = "URL DA CAPA";
+            // 
+            // lblCampAno
+            // 
+            lblCampAno.AutoSize = true;
+            lblCampAno.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCampAno.Location = new Point(29, 234);
+            lblCampAno.Name = "lblCampAno";
+            lblCampAno.Size = new Size(142, 15);
+            lblCampAno.TabIndex = 7;
+            lblCampAno.Text = "ANO DE LANÇAMENTO *";
+            // 
+            // lblCampDesc
+            // 
+            lblCampDesc.AutoSize = true;
+            lblCampDesc.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCampDesc.Location = new Point(29, 138);
+            lblCampDesc.Name = "lblCampDesc";
+            lblCampDesc.Size = new Size(72, 15);
+            lblCampDesc.TabIndex = 8;
+            lblCampDesc.Text = "DESCRIÇÃO";
+            // 
+            // lblCampTitulo
+            // 
+            lblCampTitulo.AutoSize = true;
+            lblCampTitulo.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCampTitulo.Location = new Point(29, 70);
+            lblCampTitulo.Name = "lblCampTitulo";
+            lblCampTitulo.Size = new Size(114, 15);
+            lblCampTitulo.TabIndex = 9;
+            lblCampTitulo.Text = "TITULO DO FILME *";
+            // 
+            // lblTituloForm
+            // 
+            lblTituloForm.AutoSize = true;
+            lblTituloForm.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTituloForm.ForeColor = Color.FromArgb(102, 38, 121);
+            lblTituloForm.Location = new Point(39, 32);
+            lblTituloForm.Name = "lblTituloForm";
+            lblTituloForm.Size = new Size(55, 21);
+            lblTituloForm.TabIndex = 10;
+            lblTituloForm.Text = "FILME";
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.BackColor = Color.Transparent;
+            guna2TextBox1.BorderColor = Color.DarkGray;
+            guna2TextBox1.BorderRadius = 6;
+            guna2TextBox1.CustomizableEdges = customizableEdges13;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FillColor = Color.WhiteSmoke;
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 9F);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Location = new Point(29, 468);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PlaceholderForeColor = Color.Gray;
+            guna2TextBox1.PlaceholderText = "Ex: Livre";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2TextBox1.Size = new Size(205, 36);
+            guna2TextBox1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(29, 292);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 6;
+            label1.Text = "DURAÇÂO";
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.BackColor = Color.Transparent;
+            guna2TextBox2.BorderColor = Color.DarkGray;
+            guna2TextBox2.BorderRadius = 6;
+            guna2TextBox2.CustomizableEdges = customizableEdges15;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FillColor = Color.WhiteSmoke;
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Font = new Font("Segoe UI", 9F);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Location = new Point(29, 310);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PlaceholderForeColor = Color.Gray;
+            guna2TextBox2.PlaceholderText = "Em minutos";
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            guna2TextBox2.Size = new Size(124, 36);
+            guna2TextBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(29, 450);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 15);
+            label2.TabIndex = 6;
+            label2.Text = "URL DA CAPA";
+            // 
+            // MovieFormDialog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(530, 620);
+            Controls.Add(cmbCategoria);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnSalvar);
+            Controls.Add(txtDescricao);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(txtCoverUrl);
+            Controls.Add(txtAno);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(txtTitulo);
+            Controls.Add(label1);
+            Controls.Add(lblCampCategoria);
+            Controls.Add(label2);
+            Controls.Add(lblCampCover);
+            Controls.Add(lblCampAno);
+            Controls.Add(lblCampDesc);
+            Controls.Add(lblCampTitulo);
+            Controls.Add(lblTituloForm);
+            Name = "MovieFormDialog";
+            Text = "MovieFormDialog";
+            Load += MovieFormDialog_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbCategoria;
+        private Guna.UI2.WinForms.Guna2Button btnCancelar;
+        private Guna.UI2.WinForms.Guna2Button btnSalvar;
+        private Guna.UI2.WinForms.Guna2TextBox txtDescricao;
+        private Guna.UI2.WinForms.Guna2TextBox txtCoverUrl;
+        private Guna.UI2.WinForms.Guna2TextBox txtAno;
+        private Guna.UI2.WinForms.Guna2TextBox txtTitulo;
+        private Label lblCampCategoria;
+        private Label lblCampCover;
+        private Label lblCampAno;
+        private Label lblCampDesc;
+        private Label lblCampTitulo;
+        private Label lblTituloForm;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Label label2;
+    }
+}
---
+++ Movies.Desktop/Forms/MovieFormDialog.cs
@@ -0,0 +1,56 @@
+﻿using Movies.Desktop.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movies.Desktop.Forms
+{
+    public partial class MovieFormDialog : Form
+    {
+        public CreateMovieDto? MovieDto { get; private set; }
+
+        /// <summary>
+        /// DTO preenchido quando no modo de edição (OK)
+        /// </summary>
+        public UpdateMovieDto? UpdateDto { get; private set; }
+
+        private List<CategoryResponseDto> _categorias = new();
+
+        private MovieResponseDto? _movieExistente;
+        public MovieFormDialog()
+        {
+            InitializeComponent();
+        }
+
+        public MovieFormDialog(List<CategoryResponseDto> categorias, MovieResponseDto? movie)
+        {
+            _categorias = categorias;
+            _movieExistente = movie;
+            InitializeComponent();
+        }
+
+        private void MovieFormDialog_Load(object sender, EventArgs e)
+        {
+            if (DesignMode) return;
+
+            //Configurar titulo baseado no modo (criação/edição)
+            this.Text = _movieExistente == null ? "Novo game" : "Editar game";
+            lblTituloForm.Text = _movieExistente == null ? "➕ Novo Game" : "✏️ Editar Game";
+
+            //Popula o comboBox de categorias
+            cmbCategoria.Items.Clear();
+            cmbCategoria.Items.Add("Selecione uma categoria...");
+            foreach (var cat in _categorias)
+                cmbCategoria.Items.Add(cat.Name);
+            cmbCategoria.SelectedIndex = 0;
+
+            PreencherCampos();
+        }
+    }
+}
